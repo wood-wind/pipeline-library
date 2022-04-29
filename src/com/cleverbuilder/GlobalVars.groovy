f930c24b061314de44a823e7488761cdbbff74c5
zhengjunwu <624542466@qq.com>
2022-04-29 17:20:45 +0800

    pipeline-library 测试

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -2,7 +2,9 @@
 package com.cleverbuilder
 
 class GlobalVars {
-   static String foo = "bar"
+   static String COMMIT_ID_SHORT = sh(returnStdout: true, script: 'git log --oneline -1 | awk \'{print \$1}\'')
+   static String COMMIT_ID = sh(returnStdout: true, script: 'git rev-parse  HEAD')
+   static String CREATE_TIME = sh(returnStdout: true, script: 'date "+%Y-%m-%d %H:%M:%S"')
 
    // refer to this in a pipeline using:
    //
