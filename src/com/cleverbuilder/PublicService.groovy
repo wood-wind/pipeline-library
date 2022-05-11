7ca18961b423901a6f7feee6231eb052199b74b9
zhengjunwu <624542466@qq.com>
2022-05-11 11:49:33 +0800

    pipeline-library 测试

--- src/com/cleverbuilder/PublicService.groovy
+++ src/com/cleverbuilder/PublicService.groovy
@@ -1,33 +1,18 @@
 #!/usr/bin/env groovy
 package com.cleverbuilder
 
-class PublicService {
-    String getCommitMessage() {
-        script.sh(
-            returnStdout: true,
-            script: 'git log -1 --pretty=%B HEAD',
-            label: 'Get Git commit message'
-        ).trim()
-    }
-    String getCommitId() {
-        script.sh(
-            returnStdout: true,
-            script: 'git rev-parse  HEAD',
-            label: 'Get Git commit ID'
-        ).trim()
-    }
-    String getCommitIdShort() {
-        script.sh(
-            returnStdout: true,
-            script: 'git log --oneline -1 | awk {print $1}',
-            label: 'Get Git commit ID short'
-        ).trim()
-    }
-    String createTime() {
-        script.sh(
-            returnStdout: true,
-            script: 'date "+%Y-%m-%d %H:%M:%S"',
-            label: 'Get Create Time'
-        ).trim()
-    }
+
+def getCommitMessage() {
+    script.sh(returnStdout: true, script: 'git log -1 --pretty=%B HEAD', label: 'Get Git commit message'
+    ).trim()
+}
+def getCommitId(int len=41) {
+    return sh(script: 'git rev-parse HEAD', returnStdout: true).trim()[0..len]
+}
+def getCommitIdShort(int len=6) {
+    return sh(script: 'git rev-parse HEAD', returnStdout: true).trim()[0..len]
+}
+def createTime() {
+    script.sh(returnStdout: true, script: 'date "+%Y-%m-%d %H:%M:%S"', label: 'Get Create Time'
+    ).trim()
 }
