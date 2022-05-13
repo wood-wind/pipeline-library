eb6896787a9c5141f1a470b1acdaa98d7fc0a93e
zhengjunwu <624542466@qq.com>
2022-05-13 17:36:29 +0800

    pipeline-library 测试

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -8,7 +8,7 @@ import shared.library.common.*
  * @description 通用核心共享Pipeline脚本库
  * 针对大前端Web和服务端Java、Go、Python、C++等多语言项目
  */
-def call(String type = 'web-java', Map map,Modules modules) {
+def call(String type = 'web-java', Map map,modules) {
     echo "Pipeline共享库脚本类型: ${type}, jenkins分布式节点名: 前端${map.jenkins_node_front_end} , 后端${map.jenkins_node} "
     // 应用共享方法定义
 //    changeLog = new ChangeLog()
@@ -182,7 +182,7 @@ def call(String type = 'web-java', Map map,Modules modules) {
 
 
 
-                stage('Docker For Java构建') {
+                stage('build') {
                     when {
                         beforeAgent true
                         environment name: 'DEPLOY_MODE', value: GlobalVars.release
