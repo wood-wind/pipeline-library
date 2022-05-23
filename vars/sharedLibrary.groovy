d213648884d44306f0b684aab6574302a6792e45
zhengjunwu <624542466@qq.com>
2022-05-23 16:42:03 +0800

    ci(devops): 测试

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -155,14 +155,15 @@ def call(Map map) {
                 }
 
             stage('Kubernetes deploy') {
-                when {
-        //            environment name: 'DEPLOY_MODE', value: GlobalVars.release
-        //            beforeAgent true
-                    branch 'feature-pipeline-library-v3.1.1'
-                }
+//                when {
+//        //            environment name: 'DEPLOY_MODE', value: GlobalVars.release
+//        //            beforeAgent true
+//                    branch 'feature-pipeline-library-v3.1.1'
+//                }
                 steps {
                     script {
                         echo 'deploy'
+                        sh 'echo $MODULES'
                         moduleDeployList = MODULES.split(",").findAll { it }.collect { it.trim() }
                         sh 'echo $moduleDeployList'
                         def parallelDeploy = moduleDeployList.collectEntries { key ->
