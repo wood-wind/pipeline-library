bdeb2e75f7391ac4fc7a19a82a4d2410ae8ace80
zhengjunwu <624542466@qq.com>
2022-05-26 19:20:31 +0800

    ci(devops): 测试

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -173,7 +173,21 @@ def call(Map map) {
                         for (m in moduleList) {
                             moduleStages["${m}"] = {
                                     stage("${m}") {
-                                        sh 'echo ${m}'
+                                        container("${map.pipeline_agent_lable}") {
+                                            echo 'build   ' + key
+                                            withCredentials([usernamePassword(passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME', credentialsId: "$DOCKER_CREDENTIAL_ID",)]) {
+                                                sh 'echo "$DOCKER_PASSWORD" | docker login $REGISTRY -u "$DOCKER_USERNAME" --password-stdin'
+                                                sh 'docker pull ${IMAGE1}'
+                                                sh 'docker pull ${IMAGE2}'
+                                            }
+                                            sh 'docker build --build-arg REGISTRY=$REGISTRY  --no-cache  -t $REGISTRY/$DOCKER_REPO_NAMESPACE/' + key + ':$TAG_VERSION `pwd`/' + key + '/'
+                                            withCredentials([usernamePassword(passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME', credentialsId: "$DOCKER_CREDENTIAL_ID",)]) {
+                                                sh 'echo "$DOCKER_PASSWORD" | docker login $REGISTRY -u "$DOCKER_USERNAME" --password-stdin'
+                                                sh 'docker push  $REGISTRY/$DOCKER_REPO_NAMESPACE/' + key + ':$TAG_VERSION'
+                                                sh 'docker tag  $REGISTRY/$DOCKER_REPO_NAMESPACE/' + key + ':$TAG_VERSION $REGISTRY/$DOCKER_REPO_NAMESPACE/' + key + ':latest '
+                                                sh 'docker push  $REGISTRY/$DOCKER_REPO_NAMESPACE/' + key + ':latest '
+                                            }
+                                        }
                                     }
                             }
                         }
