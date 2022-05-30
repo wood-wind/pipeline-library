0d01321d69b56230c837202dabbcdcf24d663afa
zhengjunwu <624542466@qq.com>
2022-05-30 15:20:36 +0800

    ci(devops): 测试

--- src/shared/library/common/Docker.groovy
+++ src/shared/library/common/Docker.groovy
@@ -99,6 +99,8 @@ class Docker implements Serializable {
      */
     static def pull(ctx,image) {
         //def imageFullName = "${ctx.DOCKER_REPO_NAMESPACE}/${imageName}:${imageTag}"
+        def DOCKER_USERNAME = ctx.env['DOCKER_USERNAME']
+        def DOCKER_PASSWORD = ctx.env['DOCKER_PASSWORD']
         ctx.withCredentials([ctx.usernamePassword(credentialsId: "${ctx.DOCKER_CREDENTIAL_ID}", usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
             ctx.sh """ docker login ${ctx.REGISTRY} --username=${ctx.DOCKER_USERNAME} --password=${ctx.DOCKER_PASSWORD}
                        docker pull ${image}
